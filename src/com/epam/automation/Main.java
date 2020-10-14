a416af4bf24071f848b82a402be4ddc826194ee0
Siarhei Marzan <siarhei.marzan@gmail.com>
2020-10-14 11:22:03 +0300

    my first repository

--- src/com/epam/automation/Main.java
+++ src/com/epam/automation/Main.java
@@ -1,9 +1,15 @@
 package com.epam.automation;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	String s = "Hello world!";
-	System.out.println(s);
+        // Приветствовать любого пользователя при вводе его имени через командную строку.
+        Scanner scanner = new Scanner(System.in);
+	        String name = scanner.next();
+	        System.out.println("Hello " + name);
+
     }
 }
